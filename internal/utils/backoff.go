a18a1919b76b6de48cfdf9196bfc6ec623b89c9c
Angel Uriel Chávez Morones <pertempus@MacBook-Pro-de-Angel.local>
2025-09-18 14:34:02 -0600

    feat: Add backoff

--- internal/utils/backoff.go
+++ internal/utils/backoff.go
@@ -1,1 +1,27 @@
 package utils
+
+import (
+	"time"
+)
+
+type Backoff struct {
+	base       time.Duration
+	maxRetries int
+}
+
+func NewBackoff(base time.Duration, maxRetries int) Backoff {
+	return Backoff{base: base, maxRetries: maxRetries}
+}
+
+func (b Backoff) Do(fn func(i int) error) error {
+	var err error
+	for i := 0; i <= b.maxRetries; i++ {
+		err = fn(i)
+		if err == nil {
+			return nil
+		}
+		t := time.Duration(1<<i) * b.base
+		time.Sleep(t)
+	}
+	return err
+}
